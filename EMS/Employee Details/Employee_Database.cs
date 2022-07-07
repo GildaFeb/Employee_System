79e5656f982ac8b6163b8a57d2600de708f5ea02
GildaFeb <givunuj.dleru@gmail.com>
2022-07-08 02:00:19 +0800

    Buttons: Cursor's type

--- EMS/Employee Details/Employee_Database.cs
+++ EMS/Employee Details/Employee_Database.cs
@@ -8,9 +8,31 @@ namespace EMS.Employee_Details
 {
     internal class Employee_Database
     {
+      //  SqlConnection Con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\gilda\OneDrive\Documents\EmployeeDB.mdf;Integrated Security=True;Connect Timeout=30");
+
+
         public static bool AddEmployee()
         {
+/*
             MessageBox.Show("Add Employee");
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "INSERT INTO Employee(EmployeeID, Fname,Lname,Mname, Date_joined, Birth, Sex, Address, Cnumber, email) VALUES(@empID, @fname,@lname,@mname,@date_joined,@birth,@sex, @address, @cnumber, @email)";
+            cmd.Connection = Con;
+
+            cmd.Parameters.AddWithValue("@empID", employee_id.Text);
+            cmd.Parameters.AddWithValue("@fname", first_name.Text);
+            cmd.Parameters.AddWithValue("@lname", last_name.Text);
+            cmd.Parameters.AddWithValue("@mname", middle_name.Text);
+            cmd.Parameters.AddWithValue("@date_joined", hired_date.Value.Date);
+            cmd.Parameters.AddWithValue("@birth", birth.Value.Date);
+            cmd.Parameters.AddWithValue("@sex", sex.SelectedItem.ToString());
+            cmd.Parameters.AddWithValue("@address", address.Text);
+            cmd.Parameters.AddWithValue("@cnumber", contact_number.Text);
+            cmd.Parameters.AddWithValue("@email", email.Text);
+
+            cmd.ExecuteNonQuery();
+            Con.Close();
+*/
             return true;
         }
         public static bool UpdateEmployee()
